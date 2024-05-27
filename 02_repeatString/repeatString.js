95fe0b98cc20071daffaf6de035b5418370537eb
AlexGB <82088636+agmb5@users.noreply.github.com>
2024-05-27 19:26:19 +0200

    Add exercises and solution

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(x,y) {
+    let a = "";
+    if (y<0) return"ERROR"
+    else {
+    
+    for(let i=0;i<y;i++) {
+        a = a + x  
+    }}
+    return a
 };
 
 // Do not edit below this line
