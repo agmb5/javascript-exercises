4580193770f98f30d67e771b3a72177ca3462a32
AlexGB <82088636+agmb5@users.noreply.github.com>
2024-05-30 17:03:21 +0200

    Add exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function(x,...y) {
+    for (let i of y) {
+        x=x.filter(m=>m!==i)
+    }
+    return x
 };
 
 // Do not edit below this line
