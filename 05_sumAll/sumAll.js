b1545a3b738d783fbcbc0dfcfe7edc2ae5a0b661
AlexGB <82088636+agmb5@users.noreply.github.com>
2024-05-30 22:26:27 +0200

    Add exercises solutions

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,16 @@
-const sumAll = function() {
+const sumAll = function(x,y) {
+    let z =0
+    if ((x>=0)&&(y>=0)&&(x<=y)&&(typeof y==="number")){
+        for (let i=x;i<=y;i++) {
+            z=i+z
+        }
+        return z}
+    else if ((x>=0)&&(y>=0)&&(x>y)&&(typeof y==="number")){
+        for (let i=y;i<=x;i++) {
+            z=i+z
+        }
+        return z}
+    else return "ERROR"
 
 };
 
--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
-
+const leapYears = function(x) {
+    /*if (x%100==0) {
+        if (x%400==0)
+            return true
+        else return false
+    }
+    else if (x%4==0)
+        return true
+    else return false*/
+    if (((x%100==0)&&(x%400==0))||(!(x%100==0)&&(x%4==0)))
+        return true
+    else return false
 };
 
 // Do not edit below this line
